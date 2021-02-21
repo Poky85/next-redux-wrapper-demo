db86a704044bf38141a3a752a63e31a1a16e1307
Jiri Pokorny <poky85@gmail.com>
2021-02-21 19:38:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Example Next.js app using [next-redux-wrapper](https://github.com/kirill-konshin/next-redux-wrapper) and [next-i18next](https://github.com/isaachinman/next-i18next)
+
+App reproduces problem with composing `next-redux-wrapper@7.0.0-rc.1` with another Next.js HOC (here I am using `next-i18next:@7.0.1`).
---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import App, {AppInitialProps, AppContext} from 'next/app';
+import {wrapper} from '../components/store';
+
+import {appWithTranslation} from '../../i18n'
+
+class WrappedApp extends App<AppInitialProps> {
+    public static getInitialProps = (context: any) =>
+        wrapper.getInitialAppProps(store => async ({Component, ctx}: AppContext) => {
+            // Keep in mind that this will be called twice on server, one for page and second for error page
+            store.dispatch({type: 'APP', payload: 'was set in _app'});
+
+            return {
+                pageProps: {
+                    // Call page-level getInitialProps
+                    ...(Component.getInitialProps ? await Component.getInitialProps({...ctx, store}) : {}),
+                    // Some custom thing for all pages
+                    appProp: ctx.pathname,
+                },
+            };
+        })(context);
+
+    public render() {
+        const {Component, pageProps} = this.props;
+        return <Component {...pageProps} />;
+    }
+}
+
+/**
+ * App doesn't work when we combine wrapper.withRedux() and appWithTranslation() HOCs together
+ */
+export default wrapper.withRedux(appWithTranslation(WrappedApp));
